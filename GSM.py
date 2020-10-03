9bf56d710a0c28aa55dd5bad70dadc88f4560da5
Osamah Rehman Khan <58512244+osamahkhan121@users.noreply.github.com>
2020-10-03 17:44:27 +0500

    Update GSM.py

--- GSM.py
+++ GSM.py
@@ -1,16 +1,47 @@
-from sys import argv
-import gps
-import requests
+import serial
+import RPi.GPIO as GPIO      
+import os, time
 
-#Listen on port 2947 of gpsd
-session = gps.gps("localhost", "2947")
-session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+GPIO.setmode(GPIO.BOARD)    
 
-while True :
-        rep = session.next()
-        try :
-            if (rep["class"] == "TPV") :
-             		print(str(rep.lat) + "," + str(rep.lon))
-              
-        except Exception as e :
-            print("Got exception " + str(e))
+# Enable Serial Communication
+port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=1)
+
+# Transmitting AT Commands to the Modem
+# '\r\n' indicates the Enter key
+
+port.write('AT'+'\r\n')
+rcv = port.read(10)
+print (rcv)
+time.sleep(1)
+
+port.write('ATE0'+'\r\n')      # Disable the Echo
+rcv = port.read(10)
+print (rcv)
+time.sleep(1)
+
+port.write('AT+CMGF=1'+'\r\n')  # Select Message format as Text mode 
+rcv = port.read(10)
+print (rcv)
+time.sleep(1)
+
+port.write('AT+CNMI=2,1,0,0,0'+'\r\n')   # New SMS Message Indications
+rcv = port.read(10)
+print (rcv)
+time.sleep(1)
+
+# Sending a message to a particular Number
+
+port.write('AT+CMGS="92xxxxx"'+'\r\n')
+rcv = port.read(10)
+print (rcv)
+time.sleep(1)
+
+port.write('Attendance is marked. Note that this is computer generated message using Raspberry Pi'+'\r\n')  # Message
+rcv = port.read(10)
+print (rcv)
+
+port.write("\x1A") # Enable to send SMS
+for i in range(10):
+    rcv = port.read(10)
+    print (rcv)
